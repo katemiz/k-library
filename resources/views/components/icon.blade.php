6390fff127c2e6673962bd2858a1c0265ffe64a6
Kılıç Ali Temiz <katemiz@gmail.com>
2022-06-22 20:09:26 +0300

    corrected some minor mistakes

--- resources/views/components/icon.blade.php
+++ resources/views/components/icon.blade.php
@@ -1,7 +1,7 @@
 <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="{{ $width ? $width : '24' }}"
-    height="{{ $height ? $height : '24'}}"
+    width="{{ config('constants.icons.size') }}"
+    height="{{ config('constants.icons.size') }}"
     viewBox="0 0 24 24"
     fill="{{ $fill }}">
     @includeIf("icons.$icon")
